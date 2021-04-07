4a3dda329c4b54f9f37a8b1489e4ba8b6966d831
dmmaycon <mayconmoraes.dm@gmail.com>
2021-04-06 21:40:15 -0300

    inserindo o metodo show

--- src/main/java/dev/dmmaycon/apiaula/resources/AulaResource.java
+++ src/main/java/dev/dmmaycon/apiaula/resources/AulaResource.java
@@ -1,25 +1,27 @@
 package dev.dmmaycon.apiaula.resources;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import dev.dmmaycon.apiaula.models.Aula;
+import dev.dmmaycon.apiaula.repositorys.AulaRepository;
+
 @CrossOrigin(origins = "*")
 @RestController
-@RequestMapping("/aula")
+@RequestMapping("/aulas")
 public class AulaResource {
+	
+	@Autowired
+	AulaRepository aulaRepository;
 
-	@GetMapping("/hello")
-	public List<String> listaHello() {
-		List<String> lista = new ArrayList<String>();
-		lista.add("Programação 1");
-		lista.add("Programação 2");
-		lista.add("Programação 3");
-		return lista;
+	@GetMapping
+	public List<Aula> show() {		
+		return aulaRepository.findAll();
 	}
 	
 }
