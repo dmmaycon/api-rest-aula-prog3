1588945c69b559e0ccf26698b5f193c29d236b6c
dmmaycon <mayconmoraes.dm@gmail.com>
2021-04-06 21:30:55 -0300

    inserindo o arquivo data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into Aula(nome) values('Programação 1');
+insert into Aula(nome) values('Programação 2');
+insert into Aula(nome) values('Programação 3');
+insert into Aula(nome) values('Projeto de Sistemas');
